71dc06c7c31defd9aab1fc0c6782a0207891d5d9
Hassane31 <153856866+Hassane31@users.noreply.github.com>
2026-01-21 01:37:34 +0100

    casting

--- index.php
+++ index.php
@@ -3,4 +3,9 @@
 echo 5 + (int) '5 lesson';
 echo '<br>';
 echo 5 + (int) 15.5;
+echo '<br>';
 echo gettype( 5 + (int) 15.5);
+echo '<br>';
+echo (int)(10.5 + 10.5);
+echo '<br>';
+echo gettype((int)(10.5 + 10.5));
