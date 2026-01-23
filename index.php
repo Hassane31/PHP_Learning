0d859d6e71ef08cf7ff9ba6c409e0a5952e3321a
hassane31 <hassane31@kali.SAMSUNG>
2026-01-23 22:58:16 +0100

    Predefined Variables And Test 2

--- index.php
+++ index.php
@@ -4,9 +4,11 @@
 //print_r($_SERVER);
 //echo $_SERVER["HTTP_CONNECTION"];
 //echo '</pre>';
-echo $_GET["username"];
+echo '<h1>';
+echo $_GET["user"];
+echo '</h1>';
 ?>
 <form action="" method="get" >
-<input type="text" name="username">
+<input type="text" name="user">
 <input type="submit" value= "Send">
 </form>
