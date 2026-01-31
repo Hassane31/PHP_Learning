68694fcb9ccde5f33e67cab9fb442c406278b4a0
hassane31 <hassane31@kali.SAMSUNG>
2026-01-31 22:32:39 +0100

    function Vraiable argument list

--- index.php
+++ index.php
@@ -1,22 +1,29 @@
 <?php
-//Function Default parameter value
-
-// function get_data($name,$country,$age,$adress){
-//     if($adress == ""){
-//       $adress = "Private adress ";
-//     }
-//     $line1 = "hello im $name from $country <br>";
-//     $line2 = "im $age i live in $adress";
-//      return $line1.$line2;
-// }
-// echo get_data("iness","algeria","22","");
-
-function get_data($name,$country,$age=20,$adress="Private Adress"){
-    if($adress == ""){
-      $adress = "Private adress ";
-    }
-    $line1 = "hello im $name from $country <br>";
-    $line2 = "im $age i live in $adress";
-     return $line1.$line2;
+//Function Variable arguments list
+function calculator($num1,$num2,$num3){
+ echo "Number of argument is : ".func_num_args()."<br>";
+ echo "Number inside the argument '3' is : ".func_get_arg(2);
+ echo "<pre>";
+ print_r(func_get_args());
+ echo "</pre><br>";
+ $result = 0;
+ foreach(func_get_args() as $arg):
+  $result += $arg;
+ endforeach; 
+ echo $result;
 }
-echo get_data("iness","algeria",adress:"oran");//if u want to fill jast last argemant
+calculator(43,99,22);
+echo"<br><br><br>###############################################<br><br><br>";
+
+
+//Spread syntax In JS
+function calculator2(...$nums){//make all arguments in $nums=[143,199,122]
+ print_r($nums);
+ $result2 = 0;
+ foreach($nums as $num):
+  $result2 += $num;
+ endforeach;
+ echo "<br><br>".$result2; 
+ }
+calculator2(143,199,122);
+
