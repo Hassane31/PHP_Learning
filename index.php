3cb1809d72a268163046322a973ed1459b82d3b7
Hassane31 <153856866+Hassane31@users.noreply.github.com>
2026-02-01 01:07:32 +0100

    function training and unpacking arguments

--- index.php
+++ index.php
@@ -1,29 +1,15 @@
 <?php
-//Function Variable arguments list
-function calculator($num1,$num2,$num3){
- echo "Number of argument is : ".func_num_args()."<br>";
- echo "Number inside the argument '3' is : ".func_get_arg(2);
- echo "<pre>";
- print_r(func_get_args());
- echo "</pre><br>";
- $result = 0;
- foreach(func_get_args() as $arg):
-  $result += $arg;
- endforeach; 
- echo $result;
+//Function Training And Unpacking Arguments
+
+$group_of_skills = ["html","CSS","JS","bash"];
+
+function get_data($name,$country = "Private",...$skills){
+  echo "hello $name ur country $country ur skills are :";
+  foreach($skills as $skill):
+  echo " $skill ";
+  endforeach;
 }
-calculator(43,99,22);
-echo"<br><br><br>###############################################<br><br><br>";
 
-
-//Spread syntax In JS
-function calculator2(...$nums){//make all arguments in $nums=[143,199,122]
- print_r($nums);
- $result2 = 0;
- foreach($nums as $num):
-  $result2 += $num;
- endforeach;
- echo "<br><br>".$result2; 
- }
-calculator2(143,199,122);
-
+get_data("hassane","Algeria",...$group_of_skills);
+//display
+//hello hassane ur country Algeria ur skills are : html CSS JS bash 
