9ccbf797d973d53bb6ef081f11604dc2717ec952
Hassane31 <153856866+Hassane31@users.noreply.github.com.>
2026-02-05 00:25:59 +0100

    string access and update elements

--- index.php
+++ index.php
@@ -1,29 +1,16 @@
 <?php
-//Arrow function 
+//String access And update elements
+$str = "HassaneLaabane";
 
-$say_hi = fn() => "hello from hello (function 1)";
-echo $say_hi();
+echo $str[0];//H
+echo $str[3];//s
+echo $str[7];//3
+echo '<br>';
+echo "Nbr of letters is ".strlen($str);
+echo '<br>';
+echo "Last letter is $str[-1]";//e
+echo '<br>';
+echo "Last letter is {$str[strlen($str)-1]}";//e
 
-echo "<br>###############<br>";
- 
-$say_hola = fn($someone) => "hello from hola to $someone(function 2)";
-echo $say_hola("iness");
-
-echo "<br>###############<br>";
-
-$msg = "i hope u doing well !!";
-//inheritance variable $msg without USE() in arrow function 
-$say_hi_2 = fn ($someone) => "hi $someone $msg";
-echo $say_hi_2("iness");
-
-echo "<br>###############<br>";
-
-$num= [10,12,323,233,12,12];
-
-$num_after_adding_ten = array_map(fn($item) => $item +5,$num);
-                                //array_map("anonymous function" , Tabel variable)
-echo'<br><br>--------------------<br><br>';                                
-echo "<pre>";                                
-print_r($num_after_adding_ten);
-echo "</pre>";         
-
+$str[16]="I";//index dosnt exists 
+echo $str;//" HassaneLaabane I "
