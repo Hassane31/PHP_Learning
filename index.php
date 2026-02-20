3f41d301e2c8a7b49e5ed61fbac218f493fab098
Hassane31 <153856866+Hassane31@users.noreply.github.com.>
2026-02-21 00:46:41 +0100

    Filter validate part-2

--- index.php
+++ index.php
@@ -1,38 +1,41 @@
 <?php
-//Filter var validate part-1 
+//Filtre var validate filters Part-2 
 
-$bool = "srgs";
-var_dump(filter_var($bool, FILTER_VALIDATE_BOOL,FILTER_NULL_ON_FAILURE));
-//if $bool = "any thing other True or False values it will returned NULL
+$email = "Iness@gmail.com";
 
+var_dump(filter_var($email,FILTER_VALIDATE_EMAIL,FILTER_NULL_ON_FAILURE));
+// string(15) "Iness@gmail.com"
 echo "<br>";
 
-$url = "https://www.youtube.com/";
-
-var_dump(filter_var($url,FILTER_VALIDATE_URL));
-//string(24) "https://www.youtube.com/" -> accept
-
-$url = "wqfsda";
-var_dump(filter_var($url,FILTER_VALIDATE_URL,FILTER_NULL_ON_FAILURE));
-//NULL 
+$int = "100";
+var_dump(filter_var($int,FILTER_VALIDATE_INT,FILTER_NULL_ON_FAILURE));
+// int(100)
 echo "<br>";
 
-$url = "https://www.youtube.com/?id=100";
-var_dump(filter_var($url,FILTER_VALIDATE_URL,[
-    "flags" => FILTER_NULL_ON_FAILURE | FILTER_FLAG_PATH_REQUIRED | FILTER_FLAG_QUERY_REQUIRED]));
-//string(31) "https://www.youtube.com/?id=100" -> accept the three 
+$int = "51";
+var_dump(filter_var(
+$int,FILTER_VALIDATE_INT,
+["flags" => FILTER_NULL_ON_FAILURE ,"options" => ["min_range" => 50,"max_range" => 100 ]]
+));//int(51)
+echo "<br>";
+    
+$int = 90.00;
+var_dump(filter_var(
+$int,FILTER_VALIDATE_INT,
+["flags" => FILTER_NULL_ON_FAILURE ,"options" => ["min_range" => 50,"max_range" => 100 ]]
+));//int(90)
+
+$int = "90.00";
+var_dump(filter_var(
+$int,FILTER_VALIDATE_INT,
+["flags" => FILTER_NULL_ON_FAILURE ,"options" => ["min_range" => 50,"max_range" => 100 ]]
+));//NULL
 
 
-$ip = "192.168.1.1";
-var_dump(filter_var($ip,FILTER_VALIDATE_IP,FILTER_FLAG_IPV4));
-//string(11) "192.168.1.1"
 
-
-$ip = "222";
-var_dump(filter_var($ip,FILTER_VALIDATE_IP,[
-    "flags" => FILTER_NULL_ON_FAILURE | FILTER_FLAG_IPV6]));
-// NULL
-
-$mac = "00:1A:2B:3C:4D:5E";
-var_dump(filter_var($mac,FILTER_VALIDATE_MAC));
-//  string(17) "00:1A:2B:3C:4D:5E"
+//same same with float 
+$float = "90.01";
+var_dump(filter_var(
+$float,FILTER_VALIDATE_FLOAT,
+["flags" => FILTER_NULL_ON_FAILURE ,"options" => ["min_range" => 50,"max_range" => 100 ]]
+));//float(90.0100000000000051159076974727213382720947265625)
