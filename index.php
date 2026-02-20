e7f4f7d6afa9b2bc89458af5a60e3bf16dd33ab1
Hassane31 <153856866+Hassane31@users.noreply.github.com.>
2026-02-20 23:05:30 +0100

    Array Reduce

--- index.php
+++ index.php
@@ -1,54 +1,40 @@
 <?php
-//Array map ,Array Filter 
+//Array Reduce 
 
-Function add_title($name,$last){
-    return "Mr. ".$name."_".$last;
-}
-$first_names = ["Iness","yacine","Younes"];
-$last_names = ["secret","Chaibout","Abdelaziz"];
+$nums = [10,50,23,100];
 
-echo "<pre>";
-print_r(array_map("add_title",$first_names,$last_names));
-echo "<pre>";
-    // [0] => Mr. Iness_secret
-    // [1] => Mr. yacine_Chaibout
-    // [2] => Mr. Younes_Abdelaziz
-
-echo "<pre>";
-print_r(array_map(fn($f,$l)=>"Mr $f"."_$l ",$first_names,$last_names));
-echo "<pre>";
-//     [0] => Mr Iness_secret 
-//     [1] => Mr yacine_Chaibout 
-//     [2] => Mr Younes_Abdelaziz
-
-
-
-//array filtre
-function check_nums($num){
-    return $num > 5;
-}
- 
-$nums = [1 =>3 ,2 => 4,3 => 5,4 => 6,5 => 7,6 => 8];
-echo "<pre>";
-print_r(array_filter($nums,"check_nums"));//check if the num is greater than 5 in value
-echo "</pre>";
-    // [4] => 6
-    // [5] => 7
-    // [6] => 8
-
-echo "<pre>";
-print_r(array_filter($nums,"check_nums",ARRAY_FILTER_USE_KEY));//check if the num is greater than 5 in key
-echo "</pre>";
-    // [6] => 8
-
-function check_nums2($num,$num2){
-    return $num > 5 || $num2 > 5;
+function add($n1 ,$n2){
+return $n1 + $n2;
 }
 
-echo "<pre>";
-print_r(array_filter($nums,"check_nums2",ARRAY_FILTER_USE_BOTH));//check if the num is greater than 5 in value or key
-echo "</pre>";
-    // [4] => 6
-    // [5] => 7
-    // [6] => 8
-    
+echo array_reduce($nums, "add"); //183
+
+echo array_reduce($nums, "add",100); //283 | 100 is the initial value of the accumulator
+
+
+function add2($carry ,$item){
+echo $carry . "<br>"; 
+echo $item . "<br>";
+echo $carry + $item . "<br>";
+echo "------------------<br>";   
+return $carry + $item;
+}
+echo "<br><br><br><br><br>";
+echo array_reduce($nums, "add2",100); //283 | 100 is the initial value of the accumulator
+// 100
+// 10
+// 110
+// ------------------
+// 110
+// 50
+// 160
+// ------------------
+// 160
+// 23
+// 183
+// ------------------
+// 183
+// 100
+// 283
+// ------------------
+// 283
